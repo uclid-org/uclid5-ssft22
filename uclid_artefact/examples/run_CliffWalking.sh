edcc3717c5552d3cfaedcf5f9a626b56401960ba
Elizabeth Polgreen <epolgreen@gmail.com>
2022-12-14 16:09:46 +0000

    add cliff walking example

---
+++ uclid_artefact/examples/run_CliffWalking.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Moving into openAi gym directory"
+cd OpenAIGym
+
+echo "Running uclid on cliff walking.ucl to synthesis agent... this takes a while"
+echo "uclid -y \"cvc5 --lang=sygus2\" cliff_walking.ucl"
+time uclid -y "cvc5 --lang=sygus2" cliff_walking.ucl
+
+
+echo "Running uclid on cliff walking.ucl to synthesis agent using delphi"
+echo "uclid -y \"delphi --symo\" cliff_walking.ucl"
+time uclid -y "delphi --symo" cliff_walking.ucl
+
+
+echo "Running uclid on cliff walking_verify.ucl to verify pre-filled in solution"
+echo "uclid cliff_walking_verify.ucl"
+time uclid cliff_walking_verify.ucl
