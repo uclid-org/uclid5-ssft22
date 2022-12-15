ae4e6493569e0a16c34da0629434dea219790d4b
Elizabeth Polgreen <epolgreen@gmail.com>
2022-12-15 11:26:32 +0000

    fix typo in cliffwalking script

--- uclid_artefact/examples/run_CliffWalking.sh
+++ uclid_artefact/examples/run_CliffWalking.sh
@@ -3,12 +3,12 @@
 echo "Moving into CliffWalking (OpenAI gym) directory"
 cd CliffWalking
 
-echo "Running uclid on cliff walking.ucl to synthesis agent... this takes a while"
+echo "Running uclid on cliff walking.ucl to synthesise agent using CVC5... this takes a while"
 echo "uclid -y \"cvc5 --lang=sygus2\" cliff_walking.ucl"
 time uclid -y "cvc5 --lang=sygus2" cliff_walking.ucl
 
 
-echo "Running uclid on cliff walking.ucl to synthesis agent using delphi"
+echo "Running uclid on cliff walking.ucl to synthesise agent using delphi"
 echo "uclid -y \"delphi --symo\" cliff_walking.ucl"
 time uclid -y "delphi --symo" cliff_walking.ucl
 
